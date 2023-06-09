2edc1cfeb595fa9754046a73131e791e6879797a
Sean Siddens <seansiddens@gmail.com>
2023-06-09 13:57:44 -0700

    Removed unnecessary print statements

--- src/l1-kernel-launch/kernel_launch_test.cpp
+++ src/l1-kernel-launch/kernel_launch_test.cpp
@@ -10,6 +10,7 @@
 using ordered_json = nlohmann::ordered_json;
 using namespace std::chrono;
 
+// TODO: Don't do this.
 #define DATA_DIR "/home/ssiddens/ucsc-chpl/epiphron-master/src/l1-kernel-launch/data"
 
 double calculate_average(const std::vector<double>& values) {
@@ -40,140 +41,16 @@ double calculate_std_dev(const std::vector<double>& values) {
     return numElements > 0 ? std::sqrt(squaredDifferenceSum / numElements) : 0.0;
 }
 
-void run_loop_test(easyvk::Device device) {
-	std::cout << "Running loop test...\n";
-	std::vector<uint32_t> spvCode =
-		#include "loop-test.cinit"
-	;	
-	const char* entryPoint = "litmus_test";
-
-	auto numTrialsPerTest = 64;
-	auto maxKernelWorkload = 1024 * 1024;
-	// Measure overhead with variety of kernel workloads.
-	for (int n = 2; n <= maxKernelWorkload; n *= 2) {
-		std::cout << "Dispatch size: " << n << "\n";
-
-		auto numDispatches = 16;
-
-		// Create GPU buffers.
-		auto numLoops = easyvk::Buffer(device, 1);
-		auto inputBuf = easyvk::Buffer(device, numDispatches);
-		auto outputBuf = easyvk::Buffer(device, numDispatches);
-		numLoops.store(0, n);
-
-		for (int i = 0; i < numDispatches; i++) {
-			inputBuf.store(i, i);
-			outputBuf.store(i, i);
-		}
-
-		std::vector<easyvk::Buffer> bufs = {numLoops, inputBuf, outputBuf};
-
-		auto program = easyvk::Program(device, spvCode, bufs);
-
-		// Dispatch a fixed amount
-		program.setWorkgroups(numDispatches);
-		program.setWorkgroupSize(1);
-
-		// Run the kernel.
-		program.initialize(entryPoint);
-
-		double totalOverhead = 0.0;
-		auto totalGpuTime = 0;
-		auto totalCpuTime = 0;
-		for (auto _ = 0; _ < numTrialsPerTest; _++) {
-			auto startTime = high_resolution_clock::now();
-			auto gpuTime = program.runWithDispatchTiming();
-			auto cpuTime = duration_cast<microseconds>(high_resolution_clock::now() - startTime).count();
-			totalGpuTime += gpuTime;
-			totalCpuTime += cpuTime;
-			totalOverhead += ((gpuTime / (double)1000.0) / cpuTime);
-		}
-
-
-		auto avgGpuTimeInMicroseconds = (totalGpuTime / (double)numTrialsPerTest) / 1000.0;
-		std::cout << "Average GPU time: " << avgGpuTimeInMicroseconds << "us" << std::endl;
-		auto avgCpuTimeInMicroseconds = totalCpuTime / (double)numTrialsPerTest;
-		std::cout << "Average CPU time: " << avgCpuTimeInMicroseconds << "us" << std::endl;
-
-		auto avgOverheadPerTrial = totalOverhead / (double)numTrialsPerTest;
-		std::cout << "Average overhead per trial: " <<  avgOverheadPerTrial << "\n\n";
-
-		// Cleanup.
-		program.teardown();
-		numLoops.teardown();
-		inputBuf.teardown();
-		outputBuf.teardown();
-	}
-}
-
-void run_empty_kernel_test(easyvk::Device device) {
-	std::vector<uint32_t> spvCode =
-		#include "empty-kernel.cinit"
-	;	
-	const char* entryPoint = "litmus_test";
-
-	auto numTrialsPerTest = 64;
-	auto maxKernelWorkload = 1024 * 1024;
-	// Measure overhead with variety of kernel workloads.
-	for (int n = 2; n <= maxKernelWorkload; n *= 2) {
-		std::cout << "Dispatch size: " << n << "\n";
-
-		// Create GPU buffers.
-		auto a = easyvk::Buffer(device, 1);
-		auto b = easyvk::Buffer(device, 1);
-		auto c = easyvk::Buffer(device, 1);
-
-		std::vector<easyvk::Buffer> bufs = {a, b, c};
-
-		auto program = easyvk::Program(device, spvCode, bufs);
-
-		program.setWorkgroups(n);
-		program.setWorkgroupSize(1);
-
-		// Run the kernel.
-		program.initialize(entryPoint);
-
-		double totalOverhead = 0.0;
-		auto totalGpuTime = 0;
-		auto totalCpuTime = 0;
-		for (auto _ = 0; _ < numTrialsPerTest; _++) {
-			auto startTime = high_resolution_clock::now();
-			auto gpuTime = program.runWithDispatchTiming();
-			auto cpuTime = duration_cast<microseconds>(high_resolution_clock::now() - startTime).count();
-			totalGpuTime += gpuTime;
-			totalCpuTime += cpuTime;
-			totalOverhead += ((gpuTime / (double)1000.0) / cpuTime);
-		}
-
-
-		auto avgGpuTimeInMicroseconds = (totalGpuTime / (double)numTrialsPerTest) / 1000.0;
-		std::cout << "Average GPU time: " << avgGpuTimeInMicroseconds << "us" << std::endl;
-		auto avgCpuTimeInMicroseconds = totalCpuTime / (double)numTrialsPerTest;
-		std::cout << "Average CPU time: " << avgCpuTimeInMicroseconds << "us" << std::endl;
-
-		auto avgOverheadPerTrial = totalOverhead / (double)numTrialsPerTest;
-		std::cout << "Average overhead per trial: " <<  avgOverheadPerTrial << "\n\n";
-
-		// Cleanup.
-		program.teardown();
-		a.teardown();
-		b.teardown();
-		c.teardown();
-	}
-}
-
 ordered_json run_vect_add_fixed_dispatch_test(easyvk::Device device, size_t numTrialsPerTest, size_t dispatchSize) {
 	// Maximum amount of work each kernel will do.
 	auto maxKernelWorkload = (1024 * 1024) / dispatchSize;
-	std::cout << "Max kernel workload: " << maxKernelWorkload << "\n";
-	std::cout << "Dispatch size: " << dispatchSize << "\n";
 
 	// Save results to JSON
 	std::vector<ordered_json> testData;
 
 	// Load program.
 	std::vector<uint32_t> spvCode =
-	#include "vect-add-fixed-dispatch.cinit"
+	#include "build/vect-add-fixed-dispatch.cinit"
 	;
 	const char *entryPoint = "test";
 
@@ -264,7 +141,7 @@ ordered_json run_vect_add_test(easyvk::Device device, size_t numTrialsPerTest, s
 	std::vector<ordered_json> testData;
 
 	std::vector<uint32_t> spvCode =
-	#include "vect-add.cinit"
+	#include "build/vect-add.cinit"
 	;
 	const char *entryPoint = "litmus_test";
 
@@ -346,6 +223,10 @@ int main(int argc, char* argv[]) {
 	// Initialize 
 	auto instance = easyvk::Instance(true);
 	auto physicalDevices = instance.physicalDevices();
+
+	std::cout << physicalDevices.size() << "\n";
+
+	return 0;
 	auto device = easyvk::Device(instance, physicalDevices.at(0));
 	std::cout << "Using device: " << device.properties.deviceName << "\n";
 	auto maxWrkGrpCount = device.properties.limits.maxComputeWorkGroupCount;
