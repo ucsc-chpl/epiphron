9f73c2f4d59f1417bc4bb6738a88c71bd99919c6
Sean Siddens <seansiddens@gmail.com>
2023-08-01 22:19:42 -0700

    working ticket lock impl and test bench

--- src/l2-global-barrier/global_barrier_benchmark.cpp
+++ src/l2-global-barrier/global_barrier_benchmark.cpp
@@ -35,13 +35,23 @@ double calculate_std_dev(const std::vector<double>& values) {
     return numElements > 0 ? std::sqrt(squaredDifferenceSum / numElements) : 0.0;
 }
 
+double calculate_coeff_variation(const std::vector<double>& values) {
+    double mean = calculate_average(values);
+    if (mean == 0.0) {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+    double sd = calculate_std_dev(values);
+    return (sd / mean);
+}
 
-int main(int argc, char* argv[]) {
+
+void ticket_lock_test() {
 	// Set up instance.
 	auto instance = easyvk::Instance(false);
 
     // Select device.
     auto device = easyvk::Device(instance, instance.physicalDevices().at(0));
+    std::cout << "Device name: " << device.properties.deviceName << "\n";
 
     // Loader shader code.
     std::vector<uint32_t> spvCode = 
@@ -50,7 +60,7 @@ int main(int argc, char* argv[]) {
     auto entry_point = "ticket_lock_test";
 
     auto numWorkgroups = 32;
-    auto workgroupSize = 32;
+    auto workgroupSize = 1;
 
     // Set up buffers.
     auto next_ticket_buf = easyvk::Buffer(device, 1);
@@ -64,15 +74,44 @@ int main(int argc, char* argv[]) {
         histogram_buf.store(i, 0);
     }
 
-    std::vector<easyvk::Buffer> kernelInputs = {next_ticket_buf, now_serving_buf, histogram_buf, counter_buf};
+    std::vector<easyvk::Buffer> kernelInputs = {next_ticket_buf, now_serving_buf, counter_buf, histogram_buf};
+
+    // Initialize the kernel.
+    auto program = easyvk::Program(device, spvCode, kernelInputs);
+    program.setWorkgroups(numWorkgroups);
+    program.setWorkgroupSize(workgroupSize);
+    program.initialize(entry_point);
 
 
+    // Launch kernel.
+    program.run();
 
     // Each thread increments the counter 256 times, so the value of the counter 
     // after the kernel runs should be numWorkgroups * workgroupSize * 256
     assert(counter_buf.load(0) == numWorkgroups * workgroupSize * 256);
+    std::cout << "numWorkgroups: " << numWorkgroups << "\n";
+    std::cout << "workgroupSize: " << workgroupSize << "\n";
+    std::cout << "Expected counter:  " << numWorkgroups * workgroupSize * 256 << "\n";
+    std::cout << "Global counter: " << counter_buf.load(0) << "\n";
 
-	// Cleanup instance
+    // Calculate lock fairness (cv should be around 0).
+    auto hist_values = std::vector<double>(numWorkgroups * workgroupSize);
+    for (int i = 0; i < numWorkgroups * workgroupSize; i++) {
+        hist_values.emplace_back(histogram_buf.load(i));
+    }
+    auto cv = calculate_coeff_variation(hist_values);
+    std::cout << "coeff. of variation (fairness): " << cv << "\n";
+
+	// Cleanup.
+    program.teardown();
+    next_ticket_buf.teardown();
+    now_serving_buf.teardown();
+    counter_buf.teardown();
+    histogram_buf.teardown();
 	instance.teardown();
+}
+
+int main(int argc, char* argv[]) {
+    ticket_lock_test();
 	return 0;
 }
--- src/l2-global-barrier/ticket_lock.cl
+++ src/l2-global-barrier/ticket_lock.cl
@@ -1,23 +1,26 @@
-static void unlock(__global atomic_uint* now_serving) {
-    atomic_store(now_serving, atomic_load(now_serving) + 1);
-}
-
 static void lock(__global atomic_uint* next_ticket, __global atomic_uint* now_serving) {
     uint my_ticket = atomic_fetch_add(next_ticket, 1);
     while (atomic_load(now_serving) != my_ticket) {}
 }
 
+static void unlock(__global atomic_uint* now_serving) {
+    atomic_store(now_serving, atomic_load(now_serving) + 1);
+}
+
 __kernel void ticket_lock_test(__global atomic_uint* next_ticket, 
                                __global atomic_uint* now_serving, 
-                               __global uint* histogram,
-                               __global uint* counter) {
+                               __global uint* counter,
+                               __global uint* histogram) {
+    uint x;
     for (int i = 0; i < 256; i++) {
         lock(next_ticket, now_serving);
 
         // Increment global counter.
-        (*counter)++;
+        x = *counter;
+        x++;
+        *counter = x;
 
-        // Update histogram.
+        // Update histogram to measure fairness.
         histogram[get_global_id(0)]++;
 
         unlock(now_serving);
