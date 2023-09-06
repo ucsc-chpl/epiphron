f824adecfcda276325db3a7680d38eb986952665
Sean Siddens <seansiddens@gmail.com>
2023-09-06 14:55:32 -0700

    Changed module name to match benchmark

--- src/l2-occupancy-discovery/Android.mk
+++ src/l2-occupancy-discovery/Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := global_barrier_benchmark 
+LOCAL_MODULE    := occupancy_discovery_benchmark 
 LOCAL_C_INCLUDES := ../../easyvk/src
-LOCAL_SRC_FILES := global_barrier_benchmark.cpp ../../easyvk/src/easyvk.cpp
+LOCAL_SRC_FILES := occupancy_discovery.cpp ../../easyvk/src/easyvk.cpp
 LOCAL_LDLIBS    += -lvulkan -llog
 
 include $(BUILD_EXECUTABLE)
