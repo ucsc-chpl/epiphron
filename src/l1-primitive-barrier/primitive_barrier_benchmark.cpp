caa90b7cde2648710d4e0a1a7cc9a598eba58f48
Sean Siddens <seansiddens@gmail.com>
2023-11-14 09:14:30 -0800

    Modifying local scratchbad buffer size from host

--- src/l1-primitive-barrier/primitive_barrier_benchmark.cpp
+++ src/l1-primitive-barrier/primitive_barrier_benchmark.cpp
@@ -13,6 +13,53 @@
 using ordered_json = nlohmann::ordered_json;
 using namespace std::chrono;
 
+// SPIR-V magic number to verify the binary
+const uint32_t SPIRV_MAGIC = 0x07230203;
+// NOTE: This function modifies the first OpConstant instruction it finds with a
+// value of LOCAL_MEM_SIZE (defined below). It does no semantic check of type or 
+// variable name, so ensure the constant you want to modify doens't conflict with
+// any previously defined constant (i.e ensure that the first #define constant w/ value
+// 1024 is at the top of the OpenCL file).
+const uint32_t LOCAL_MEM_SIZE = 1024; 
+void modifyLocalMemSize(std::vector<uint32_t>& spirv, uint32_t newValue) {
+    if(spirv.size() < 5) {
+        std::cerr << "Invalid SPIR-V binary." << std::endl;
+        return;
+    }
+
+    if(spirv[0] != SPIRV_MAGIC) {
+        std::cerr << "Not a SPIR-V binary." << std::endl;
+        return;
+    }
+
+    // Iterate through SPIR-V instructions
+    // https://github.com/KhronosGroup/SPIRV-Guide/blob/master/chapters/parsing_instructions.md
+    size_t i = 5;  // skip SPIR-V header
+    while(i < spirv.size()) {
+        uint32_t instruction = spirv[i];
+        uint32_t length = instruction >> 16;
+        uint32_t opcode = instruction & 0x0ffffu;
+
+        // Opcode source: https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpConstant
+        if(opcode == 43) { // OpConstant
+            uint32_t resultType = spirv[i+1];
+            uint32_t resultId = spirv[i+2];
+            uint32_t constantValue = spirv[i+3];
+
+            // This is a simplistic check.
+            // Doesn't verify the type and name (through debug info)
+            if(constantValue == LOCAL_MEM_SIZE) {
+                spirv[i+3] = newValue;
+                return;
+            }
+        }
+
+        i += length; // move to next instruction
+    }
+
+    std::cerr << "Did not modify any instructions when parsing the SPIR-V module!\n";
+}
+
 
 double calculate_average(const std::vector<double>& values) {
     double sum = 0.0;
@@ -56,11 +103,13 @@ std::vector<int> getWorkgroupSizes() {
 uint32_t getOccupancy(easyvk::Instance instance,
                       easyvk::Device device,
 					  size_t numTrials,
-					  size_t workgroupSize) {
+					  size_t workgroupSize,
+					  size_t localMemSize) {
 	// Load the kernel.
 	std::vector<uint32_t> kernelCode = 
 	#include "build/primitive_barrier_occupancy.cinit"
 	;
+    modifyLocalMemSize(kernelCode, localMemSize);
 
 	// All bencmarks are contained in the single kernel and we can specify which 
 	// one to run by selecting the entry point.
@@ -75,10 +124,9 @@ uint32_t getOccupancy(easyvk::Instance instance,
 	auto numWorkgroups = 1024;
 
 	// Set up kernel inputs.
-	auto bufSize =  static_cast<uint32_t>(8192); // buf of uint32_t's
-	auto buf = easyvk::Buffer(device, bufSize, sizeof(uint32_t));
+	auto buf = easyvk::Buffer(device, localMemSize, 1); 
 	auto buf_size = easyvk::Buffer(device, 1, sizeof(uint32_t));
-	buf_size.store<uint32_t>(0, bufSize);
+	buf_size.store<uint32_t>(0, localMemSize);
 	auto num_iters = easyvk::Buffer(device, 1, sizeof(uint32_t));
 	num_iters.store<uint32_t>(0, 2);
 	auto count_buf = easyvk::Buffer(device, 1, sizeof(uint32_t));
@@ -134,7 +182,8 @@ uint32_t getOccupancy(easyvk::Instance instance,
 ordered_json primitiveBarrierBenchmark(easyvk::Instance instance,
                                        size_t deviceIndex,
 								       size_t numTrials,
-								       size_t numIters) {
+								       size_t numIters,
+									   size_t bufferSize) {
 
 	// Select device to use from the provided device index.
 	auto device = easyvk::Device(instance, instance.physicalDevices().at(deviceIndex));
@@ -146,7 +195,8 @@ ordered_json primitiveBarrierBenchmark(easyvk::Instance instance,
 	std::vector<uint32_t> kernelCode = 
 	#include "build/primitive_barrier.cinit"
 	;
-
+	modifyLocalMemSize(kernelCode, bufferSize);
+	
 	// All bencmarks are contained in the single kernel and we can specify which 
 	// one to run by selecting the entry point.
 	std::vector<const char*> entryPoints = {
@@ -170,14 +220,13 @@ ordered_json primitiveBarrierBenchmark(easyvk::Instance instance,
 		std::vector<ordered_json> benchmarkResults;
 		for (const auto &n : workgroupSizes) {
 			// Get occupancy bound.
-			uint occupancyBound = getOccupancy(instance, device, 8, n);
+			uint occupancyBound = getOccupancy(instance, device, 8, n, bufferSize);
 
 			// Set up kernel inputs.
-			auto bufSize =  static_cast<uint32_t>(8192); // buf of uint32_t's
-			auto buf = easyvk::Buffer(device, bufSize, sizeof(uint32_t));
+			auto buf = easyvk::Buffer(device, bufferSize, 1);
 			auto buf_size = easyvk::Buffer(device, 1, sizeof(uint32_t));
 			auto num_iters = easyvk::Buffer(device, 1, sizeof(uint32_t));
-			buf_size.store<uint32_t>(0, bufSize);
+			buf_size.store<uint32_t>(0, bufferSize);
 			num_iters.store<uint32_t>(0, numIters);
 
 			std::vector<easyvk::Buffer> kernelInputs = {buf, buf_size, num_iters};
@@ -233,25 +282,27 @@ ordered_json primitiveBarrierBenchmark(easyvk::Instance instance,
 int main(int argc, char* argv[]) {
 	// BENCHMARK PARAMETERS
 	auto numTrials = 32;
-	auto numIters = 1024 * 4; // # of iterations to run kernel loop
-	auto deviceIndex = 0;
+	auto numIters = 1024 * 16; // # of iterations to run kernel loop
+	auto deviceIndex = 2;
 
 	// Run benchmark on every availible device.
 	auto instance = easyvk::Instance(USE_VALIDATION_LAYERS);
 	// Query device properties.
 	auto device = easyvk::Device(instance, instance.physicalDevices().at(deviceIndex));
-	auto deviceName = device.properties.deviceName;
+	auto bufferSize = 32768; // Size to set the local and global scratchpad buffers.
 
 	// "warm up" the GPU by giving it some initial work. If this is not done the 
 	// results of the first benchmark that is run will skewed for some reason.	
 	// auto _ = primitiveBarrierBenchmark(instance, deviceIndex, 32, 1024);
 
-	auto primitiveBarrierBenchmarkResult = primitiveBarrierBenchmark(instance, deviceIndex, numTrials, numIters);
+	auto primitiveBarrierBenchmarkResult = primitiveBarrierBenchmark(instance, deviceIndex, numTrials, numIters, bufferSize);
 
+	auto deviceName = device.properties.deviceName;
 	primitiveBarrierBenchmarkResult["benchmarkName"] = "primitiveBarrier";
 	primitiveBarrierBenchmarkResult["deviceName"] = deviceName;
 	primitiveBarrierBenchmarkResult["numTrials"] = numTrials;
 	primitiveBarrierBenchmarkResult["numIters"] = numIters;
+	primitiveBarrierBenchmarkResult["bufferSize"] = bufferSize;
 
 	// Write results to file.
 	// Get current time
