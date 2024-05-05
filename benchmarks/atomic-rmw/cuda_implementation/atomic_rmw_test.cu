fd65efcb88c9e0fbaf0b683e798a5c8bed21ac04
Gurpreet Dhillon <gudhillon@gmail.com>
2024-05-05 14:25:07 -0700

    occupancy and cpu timings

--- benchmarks/atomic-rmw/cuda_implementation/atomic_rmw_test.cu
+++ benchmarks/atomic-rmw/cuda_implementation/atomic_rmw_test.cu
@@ -23,32 +23,31 @@ ofstream benchmarkData;
         }                                                                   \
     }
 
-__global__ void fetch_add(uint *res, uint iter, uint *indexes) {
+#pragma nv_exec_check_disable
+__global__ void fetch_add(uint *res, uint iter, uint *mapping) {
     int global_id = blockIdx.x * blockDim.x + threadIdx.x;
-    uint index = indexes[global_id];
+    uint index = mapping[global_id];
 
     for (uint i = 0; i < iter; i++) {
         atomicAdd(&res[index], 1);
     }
 }
 
-
 void run(uint32_t workgroups, uint32_t workgroup_size, uint32_t test_iters) {
     
     list<uint32_t> test_values;
-    uint32_t tmp;
     uint32_t error_count = 0;
-    if (workgroups * workgroup_size > 1024) tmp = 1024;
-    else tmp = workgroup_size * workgroups;
+
+    uint32_t tmp = (workgroups * workgroup_size > 1024) ? 1024 : workgroup_size * workgroups;
 
     for (uint32_t i = 1; i <= tmp; i *= 2) {
         test_values.push_back(i);  
     } 
 
-    for (auto it1 = test_values.begin(); it1 != test_values.end(); ++it1) {
-        for (auto it2 = test_values.begin(); it2 != test_values.end(); ++it2) {
-            uint32_t contention = *it1;
-            uint32_t padding = *it2;
+    for (auto c = test_values.begin(); c != test_values.end(); c++) {
+        for (auto p = test_values.begin(); p != test_values.end(); p++) {
+            uint32_t contention = *c;
+            uint32_t padding = *p;
             benchmarkData << "(" + to_string(contention) + ", " + to_string(padding) + ", ";
 
             float observed_rate = 0.0;
@@ -57,29 +56,19 @@ void run(uint32_t workgroups, uint32_t workgroup_size, uint32_t test_iters) {
             uint32_t global_work_size = workgroup_size * workgroups;
             const int size = ((global_work_size) * padding) / contention;
 
-            // Host input and output vectors
-            uint32_t *h_strat;
-            // Host output vector
-            uint32_t *h_result;
+            uint32_t *h_strat, *h_result;
+            uint32_t *d_strat, *d_result;
 
-            // Device input vectors
-            uint32_t *d_strat;
-            // Device output vector
-            uint32_t *d_result;
-            
             size_t bytes_strat = global_work_size * sizeof(uint32_t);
             size_t bytes_result = size * sizeof(uint32_t);
 
-            // Allocate memory for each vector on host
             h_strat = (uint32_t *)malloc(bytes_strat);
             h_result = (uint32_t *)malloc(bytes_result);
 
-            // Allocate memory for each vector on GPU
             cudaMalloc(&d_strat, bytes_strat);
             cudaMalloc(&d_result, bytes_result);
 
             for (int i = 0; i < global_work_size; i += 1) h_strat[i] = (i / contention) * padding;
-            // Copy host vectors to device
             cudaMemcpy(d_strat, h_strat, bytes_strat, cudaMemcpyHostToDevice);
 
             while(true) { 
@@ -87,37 +76,31 @@ void run(uint32_t workgroups, uint32_t workgroup_size, uint32_t test_iters) {
                 cudaMemcpy(d_result, h_result, bytes_result, cudaMemcpyHostToDevice);
                 
                 observed_rate = 0.0;
+                cudaEvent_t start, stop;
+                cudaEventCreate(&start);
+                cudaEventCreate(&stop);
                 for (int i = 1; i <= test_iters; i++) {
                     float kernelTime = 0;
-                    cudaEvent_t start, stop;
-                    cudaEventCreate(&start);
-                    cudaEventCreate(&stop);
-
-                    // Execute the kernel
                     cudaEventRecord(start);
                     CUDA_SAFECALL((fetch_add<<<workgroups, workgroup_size>>>(d_result, rmw_iters, d_strat)));
                     cudaEventRecord(stop);
                     cudaEventSynchronize(stop);
                     cudaEventElapsedTime(&kernelTime, start, stop);
-
-                    cudaEventDestroy(start);
-                    cudaEventDestroy(stop);
-                    cudaDeviceSynchronize();
                     observed_rate += (float(rmw_iters * workgroup_size * workgroups) / float((kernelTime / (double) 1000.0))); 
                 }
+                cudaEventDestroy(start);
+                cudaEventDestroy(stop);
                 observed_rate /= float(test_iters);
                 
                 if (isinf(observed_rate)) rmw_iters *= 2;
                 else break;
             }
             benchmarkData << to_string(observed_rate) + ")" << endl;
-            cudaMemcpy(h_result, d_result, bytes_result, cudaMemcpyDeviceToHost);
 
+            cudaMemcpy(h_result, d_result, bytes_result, cudaMemcpyDeviceToHost);
             for (int access = 0; access < size; access += padding) {
                 if (h_result[access] != rmw_iters * test_iters * contention) error_count += 1;
             }
-
-            // Release device and host memory
             cudaFree(d_result);
             cudaFree(d_strat);
             free(h_result);
@@ -138,19 +121,27 @@ int main(int argc, char *argv[]) {
         cerr << "Failed to open the output file." << endl;
         return 1;
     }
-    int device;
+
+    int device, numBlocks, activeWarps;
     cudaGetDevice(&device);
 
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, device);
 
-    uint32_t test_iters = 64;
-    uint32_t workgroup_size =  deviceProp.maxThreadsPerBlock;
-    uint32_t workgroups = deviceProp.multiProcessorCount * (deviceProp.maxThreadsPerMultiProcessor / workgroup_size);
+    uint32_t test_iters = 32;
+    int blockSize = deviceProp.maxThreadsPerBlock;
 
-    benchmarkData << to_string(workgroup_size) + "," + to_string(workgroups) + ":" + deviceProp.name + ", contiguous_access: atomic_fa_relaxed\n";
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(
+        &numBlocks,
+        fetch_add,
+        blockSize,
+        0);
+    activeWarps = numBlocks * blockSize / deviceProp.warpSize;
 
-    run(workgroups, workgroup_size, test_iters);
+    cout << "Occupancy: " << (double)activeWarps / (deviceProp.maxThreadsPerMultiProcessor / deviceProp.warpSize) * 100 << "%" << endl;
+
+    benchmarkData << to_string(blockSize) + "," + to_string(activeWarps) + ":" + deviceProp.name + ", contiguous_access: atomic_fetch_add\n";
+    run(activeWarps, blockSize, test_iters);
 
     benchmarkData.close();
 
