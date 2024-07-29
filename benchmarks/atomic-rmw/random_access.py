acff73ed36b0c987c91c88c835c0d97aeabe03ec
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-07-29 16:26:22 -0700

    Reordering random access results, making colors in random access graph match vendor

--- benchmarks/atomic-rmw/random_access.py
+++ benchmarks/atomic-rmw/random_access.py
@@ -27,6 +27,11 @@ def extract_coordinates(filename):
                 current_title = line.strip()
     return coordinates
 
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
 def main():
     plt.style.use(['science','no-latex'])
     plt.rcParams['xtick.minor.size'] = 0
@@ -45,11 +50,14 @@ def main():
         print(f"Processing '{filename}'...")
         # Extract coordinates from the file
         coordinates = extract_coordinates(os.path.join(read_folder, filename))
-        titles = set(coord[2] for coord in coordinates)
-        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+        titles = [coord[2] for coord in coordinates]
+        # Using scienceplots default colors, but in order of AMD/NVIDIA/Intel/Apple/Samsung/ARM
+        colors = ['#FF2C00', '#00B945', '#0C5DA5', '#FF9500', '#845B97', '#845B97']
         lines_data = {}
 
-        for title in sorted(titles):
+        titles = f7(titles)
+
+        for title in titles:
             if "random_access:" in title:
                 graph_coordinates = [c[0:2] for c in coordinates if c[2] == title]
                 lines_data[vendor_name(title)] = graph_coordinates
@@ -59,7 +67,7 @@ def main():
         for i, (title, graph_coordinates) in enumerate(lines_data.items()):
             contention_values = [coord[0] for coord in graph_coordinates]
             throughput_values = [coord[1] / graph_coordinates[0][1] for coord in graph_coordinates]
-            plt.plot(contention_values, throughput_values, marker='o', linestyle='-', label=title, markersize=6)
+            plt.plot(contention_values, throughput_values, marker='o', linestyle='-', label=title, markersize=6, color=colors[i])
 
         x_ticks = [2 ** i for i in range(0, 11)]  # Powers of 2 from 1 to 1024
         x_labels = [str(2 ** i) for i in range(0, 11)]
@@ -74,6 +82,8 @@ def main():
         save_folder = "graphs"
         os.makedirs(save_folder, exist_ok=True)
 
+        plt.show()
+        
         svgfilename = os.path.join(save_folder, filename.removesuffix(".txt") + ".svg")
         pngfilename = os.path.join(save_folder, filename.removesuffix(".txt") + ".png")
         
