6a89d3f1cc87878ae7e4f8119d55ca36c540be0d
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-08-06 14:57:00 -0700

    Some changes to graph rendering for device_bar, updating Makefiles for Windows compilation, lowering test time to 500ms cause of NVIDIA Windows kernel interrupt time

--- benchmarks/atomic-rmw/device_bar.py
+++ benchmarks/atomic-rmw/device_bar.py
@@ -23,19 +23,14 @@ for f in files:
 diffs = [maxs[i] / mins[i] for i in range(len(devices))]
 
 plt.style.use('science')
+plt.rcParams['svg.fonttype'] = 'none'
+plt.rcParams['font.family'] = 'Linux Libertine'
+plt.rcParams['text.usetex'] = False
 
-colors = ['red', 'lawngreen', 'deepskyblue', 'lightslateblue']
-colors = np.empty([4, 4], dtype=np.float32)
-colors[0] = [1, 0, 0, 1]
-colors[1] = [0.486, 0.988, 0, 1]
-colors[2] = [0, 0.749, 1, 1]
-colors[3] = [0.69, 0.769, 0.87, 1]
+colors = ['#FF2C00', '#00B945', '#0C5DA5', '#FF9500']
+dark_colors = ['#CC2200', '#008732', '#084173', '#CC7700']
 
-dark_colors = np.empty([4, 4], dtype=np.float32)
-for c in range(len(colors)):
-    dark_colors[c] = np.clip(np.append(colors[c][:3]-0.3, [1]), 0, 1)
-
-fig, ax = plt.subplots(figsize=(4, 4))
+fig, ax = plt.subplots(figsize=(4, 3))
 
 plt.bar(x-0.15, mins, 0.3, color=dark_colors, edgecolor='black')
 plt.bar(x+0.15, maxs, 0.3, color=colors, edgecolor='black')
@@ -43,7 +38,7 @@ plt.xticks(x, devices)
 plt.xlabel('Devices')
 plt.ylabel('Atomic Operations per Microsecond')
 plt.yscale('log')
-plt.ylim(1, (math.e ** 1.05) * max(maxs))
+plt.ylim(1, (math.e ** 1.4) * max(maxs))
 
 rects = ax.patches
 labels = (['Min'] * len(devices)) + (['Max'] * len(devices))
@@ -54,13 +49,13 @@ for rect, label in zip(rects, labels):
 plt.savefig('graphs/throughput-diffs.png', bbox_inches='tight')
 plt.savefig('graphs/throughput-diffs.svg', format='svg', bbox_inches='tight')
 
-fig, ax = plt.subplots(figsize=(4, 4))
+fig, ax = plt.subplots(figsize=(4, 3))
 plt.xticks(x, devices)
 plt.xlabel('Devices')
 plt.ylabel('Maximum throughput falloff')
 plt.bar(x, diffs, 0.7, color=colors, edgecolor='black')
 plt.yscale('log')
-plt.ylim(1, (math.e ** 1.05) * max(diffs))
+plt.ylim(1, (math.e ** 1.00) * max(diffs))
 
 rects = ax.patches
 labels = [f'{int(diffs[i])}x' for i in range(len(rects))]
